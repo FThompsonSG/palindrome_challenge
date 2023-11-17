2ccf37b3c74b802e819a26e0a03e17626bcf5416
Fergus Thompson <FThompson@spartaglobal.com>
2023-11-17 15:54:49 +0000

    Written tests but need to resolve bug with assertTrue test method

--- src/test/java/com/sparta/ft/PalindromeCheckerTest.java
+++ src/test/java/com/sparta/ft/PalindromeCheckerTest.java
@@ -1,18 +1,24 @@
 package com.sparta.ft;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromeCheckerTest {
 
+    PalindromeChecker palindromeChecker = new PalindromeChecker();
+
     @Test
-    void checkMethod() {
+    @DisplayName("Test to check method returns true")
+    void checkMethodTrueTest() {
+        assertTrue(palindromeChecker.checkMethod("BOB"));
     }
 
     @Test
-    void checkLongestPalindrome() {
+    @DisplayName("Test to check method returns longest palindrome")
+    void checkLongestPalindromeTest() {
+        Assertions.assertEquals("DEEEED", palindromeChecker.checkLongestPalindrome("DOOD IS THE DEEEED AND BOB HAS"));
     }
-
 
 }
